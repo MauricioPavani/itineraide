7cf73a961527f793821fbb8c74b7fbbdaeb07394
Mauricio Pavani <mauricio_pavani@hotmail.com>
2025-01-02 13:41:48 -0300

    CLI implementado

--- itineride/itineride.py
+++ itineride/itineride.py
@@ -1,2 +1,45 @@
-def list_open_rides():
-    return list()
+from datetime import datetime
+
+import ride_optimization
+import ride_request
+from InquirerPy import inquirer
+
+
+def cli():
+    parks = ride_request.list_parks.keys()
+    selected_park = inquirer.select(
+        message='Pick a park:', choices=parks
+    ).execute()
+
+    rides = ride_request.list_rides(ride_request.list_parks[selected_park])
+
+    rated_rides = dict()
+    print('Type directional key and rate the ride:')
+    for ride in rides:
+        rate = inquirer.number(
+            message=ride + ':', min_allowed=1, max_allowed=10
+        ).execute()
+
+        rated_rides[ride] = {'wait_time': rides[ride], 'will_index': int(rate)}
+
+    start_park = inquirer.text('Type start park (hh:mm): ').execute()
+    end_park = inquirer.text('Type end park (hh:mm): ').execute()
+
+    try:
+        format_time = '%H:%M'
+        start_park = datetime.strptime(start_park, format_time)
+        end_park = datetime.strptime(end_park, format_time)
+
+        diff = end_park - start_park
+        park_time = diff.total_seconds() / 60
+    except ():
+        park_time = 300
+
+    selected_rides = ride_optimization.ride_allocation(rated_rides, park_time)
+    print('Selected rides:')
+    for ride in selected_rides:
+        print(ride)
+
+
+if __name__ == '__main__':
+    cli()
--- tests/test_itineride.py
+++
@@ -1,6 +0,0 @@
-from itineride import itineride
-
-
-def test_return_type_list_open_rides():
-    result = itineride.list_open_rides()
-    assert type(result) == type(list())
